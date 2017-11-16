ec146ef95b8f86cd1ad7b3178530f9c806c27bb1
Andrias Sundskarð <andriassundskard@gmail.com>
2017-11-16 20:22:09 +0100

    Working..

---
+++ server.js
@@ -0,0 +1,28 @@
+﻿var 	app = require("http").createServer(handler),
+	io = require("socket.io")(app),
+	fs = require("fs")
+
+app.listen(8000);
+
+function handler(req, res) {
+	fs.readFile(__dirname+"/index.html", function(err, data) {
+		if (err) {
+			console.log(err);
+			res.writeHead(500);
+			return res.end("Error loading index.html");
+		}
+		res.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+		res.end(data);
+	});
+}
+
+io.on("connection", function(socket) {
+	setInterval(function(){
+		socket.emit("news", { hello: "world" });
+	}, 5000);
+	socket.on("my other event", function (data) {
+		console.log(data);
+	});
+});
